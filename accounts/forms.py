6f898b0a09ff133a5919a9a68803e8f6bd075f0e
jaypee15 <okoyejohnpaul15@gmail.com>
2024-02-05 12:14:55 +0100

    refactor: customuser model and form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,45 +1,42 @@
-from allauth.account.forms import SignupForm
-from allauth.socialaccount.forms import SignupForm as SocialSignupForm
-from django.contrib.auth import forms as admin_forms
-from django.contrib.auth import get_user_model
-from django.forms import EmailField
-from django.utils.translation import gettext_lazy as _
-
-User = get_user_model()
+from django import forms
+from .models import CustomUser
+from django.utils.translation import ugettext_lazy as _
 
 
-class UserAdminChangeForm(admin_forms.UserChangeForm):
-    class Meta(admin_forms.UserChangeForm.Meta):
-        model = User
-        field_classes = {"email": EmailField}
+class BasicFormStyle(forms.ModelForm):
+    """define the base and the form name"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for key, field in self.fields.items():
+            field.widget.attrs['class'] = "form-control"
 
 
-class UserAdminCreationForm(admin_forms.UserCreationForm):
-    """
-    Form for User Creation in the Admin Area.
-    To change user signup, see UserSignupForm and UserSocialSignupForm.
-    """
+class ProfileUpdateForm(BasicFormStyle):
 
-    class Meta(admin_forms.UserCreationForm.Meta):
-        model = User
-        fields = ("email",)
-        field_classes = {"email": EmailField}
-        error_messages = {
-            "email": {"unique": _("This email has already been taken.")},
+    class Meta():
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'email', 'phone', 'last_login']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'placeholder': _("Firstname"),
+                                                 'name': 'first_name',
+                                                 'type': 'text',
+                                                 'aria-required': 'true',
+                                                 'minlength': '2'}),
+            'last_name': forms.TextInput(attrs={'placeholder': _("Lastname"),
+                                                'name': 'last_name',
+                                                'type': 'text',
+                                                'aria-required': 'true',
+                                                'minlength': '2'}),
+            'email': forms.TextInput(attrs={'placeholder': _("Email"),
+                                            'name': 'email',
+                                            'type': 'email',
+                                            'aria-required': 'true',
+                                            'minlength': '2'}),
+            'phone': forms.TextInput(attrs={'placeholder': _("Phone"),
+                                            'name': 'phone',
+                                            'type': 'phone',
+                                            'aria-required': 'true',
+                                            'minlength': '2'}),
+
         }
-
-
-class UserSignupForm(SignupForm):
-    """
-    Form that will be rendered on a user sign up section/screen.
-    Default fields will be added automatically.
-    Check UserSocialSignupForm for accounts created from social.
-    """
-
-
-class UserSocialSignupForm(SocialSignupForm):
-    """
-    Renders the form when user has signed up using social accounts.
-    Default fields will be added automatically.
-    See UserSignupForm otherwise.
-    """
